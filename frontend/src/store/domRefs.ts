e4d025113a084578e554e8b80ae1d53a0f1bad1e
SadeghPM <SadeghPM@users.noreply.github.com>
2026-07-06 14:06:29 +0330

    clean the project

--- frontend/src/store/domRefs.ts
+++
@@ -1,7 +0,0 @@
-export const domRefs = {
-  messageInput: null as HTMLTextAreaElement | null,
-  fileInput: null as HTMLInputElement | null,
-  newChatSearchInput: null as HTMLInputElement | null,
-  avatarInput: null as HTMLInputElement | null,
-  messagesContainer: null as HTMLDivElement | null,
-};
